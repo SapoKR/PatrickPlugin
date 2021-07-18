c577f6c5477869495b2a23ec788be2a358384e4e
PyBsh <rootpi@icloud.com>
2021-07-18 14:41:37 +0900

    init

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-# Paper Developemnt Sample Plugin
+# PatrickKR Plugin
+![2021-07-17_23 52 25](https://user-images.githubusercontent.com/59782214/126057019-5c7fa1db-3768-45e1-9adc-cf91aea3cca5.png)
 
-## Environment
+## Based on [hyeon-sample-plugin](https://github.com/qogusdn1017/hyeon-sample-plugin)
+
+### Environment
 
 - [Paper 1.17](https://papermc.io/downloads) dependencies
 - [Tap](https://github.com/monun/tap) dependencies
---
+++ src/main/kotlin/com/github/pybsh/PatrickEvent.kt
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2021 BaeHyeonWoo
+ *
+ *  Licensed under the General Public License, Version 3.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://opensource.org/licenses/gpl-3.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.github.pybsh
+
+import net.kyori.adventure.text.Component
+import org.bukkit.Bukkit.getServer
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.Plugin
+
+
+/***
+ * @author BaeHyeonWoo
+ */
+
+class PatrickEvent : Listener {
+    private fun getInstance(): Plugin {
+        return PatrickPluginMain.instance
+    }
+
+
+    @EventHandler
+    fun onPlayerJoin(e: PlayerJoinEvent) {
+        val scheduler = getServer().scheduler
+        scheduler.scheduleSyncDelayedTask(getInstance(), {
+            e.player.kick(Component.text("3초 무적을 사용합니다."))
+        }, 80L) //3초 + 입장 딜레이 1초
+    }
+}
